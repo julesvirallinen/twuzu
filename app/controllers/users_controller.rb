5417fc0212ce6b41179b67ca4ff3c4bc50811500
Julius Uusinarkaus <julius@uusinarkaus.com>
2017-05-18 23:58:21 +0300

    Only admin can view list of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def index
+    unless current_user.admin
+      redirect_to root_path, alert: 'Not permitted'
+    end
     @users = User.all
   end
 
