e25879de17d230d32f30650d45b2a88207a064a9
Julius Uusinarkaus <julius@uusinarkaus.com>
2017-05-18 19:46:44 +0300

    Adds index and single page for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+  # get 'users/index'
+  root 'tzeets#index'
+  match '/users', to: 'users#index', via: 'get'
+  match '/users/:id', to: 'users#show', via: 'get'
+  # devise_for :users, :path_prefix => 'd'
+  # resources :users, :only =>[:show]
   resources :tzeets
   devise_for :users
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'logout', to: 'devise/sessions#destroy'
+    get 'register', to: 'devise/registrations#new'
+
+  end
+
+
 end
